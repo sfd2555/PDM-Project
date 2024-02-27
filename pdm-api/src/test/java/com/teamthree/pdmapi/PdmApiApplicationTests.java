ba97fa783a0bac0a92ca7ba6ecdeef3cfa6f90fe
Sean Droll <sfd2555@g.rit.edu>
2024-02-27 16:19:00 -0500

    Initial

---
+++ pdm-api/src/test/java/com/teamthree/pdmapi/PdmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamthree.pdmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
