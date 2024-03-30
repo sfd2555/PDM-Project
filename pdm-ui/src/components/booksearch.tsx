42ce43917e88d74ae0f1e16813c40d9f66eaa43d
Sayajiaji <sayajiaji@gmail.com>
2024-03-30 13:44:54 -0400

    booksearch information

--- pdm-ui/src/components/booksearch.tsx
+++ pdm-ui/src/components/booksearch.tsx
@@ -6,7 +6,6 @@ import { addToCollection } from "../services/collectionservice"
 import Select from "react-select"
 
 export const BookSearch = ({book, collectionId} : {book: RefinedBook, collectionId:string}) => {
-    let initialValue: Format[] = []
     let [selectedFormat, setSelectedFormat] = useState("")
 
     const handleSubmit = () => {
@@ -14,9 +13,15 @@ export const BookSearch = ({book, collectionId} : {book: RefinedBook, collection
         addToCollection(collectionId, book.bookId, selectedFormat)
     }
 
+    console.log(selectedFormat)
     return(
         <div>
             <h3>{book.bookTitle}</h3>
+            Genre(s): {book.genres.map(g => g.genreName).join(", ")}
+            <br/>
+            Publishing Date: {book.formats[0]?.releaseDate}
+            <br/>
+            Contributors: {book.contributors.map(g => `${g.contributorName} (${g.contributorType})`).join(", ")}
             <form onSubmit={handleSubmit}>
                 <select value={selectedFormat} onChange={(e) => {
                     e.preventDefault();
